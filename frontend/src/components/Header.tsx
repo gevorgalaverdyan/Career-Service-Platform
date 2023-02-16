c78787a56a2045c72acee98c94c0f391f4c305f4
gevorgalaverdyan <gevorgalaverdyanmail@gmail.com>
2023-02-16 13:22:34 -0500

    fixed header name

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -7,7 +7,7 @@ function Header() {
   return (
     <header className='header'>
       <div className='logo'>
-        <Link to={'/'}>Support Desk</Link>
+        <Link to={'/'}>CareersConcordia</Link>
       </div>
       <ul>
         <>
