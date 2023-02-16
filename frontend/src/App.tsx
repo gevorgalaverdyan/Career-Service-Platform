f09acecbde47d41bb02eb6e0b2f20a6498f6b488
gevorgalaverdyan <gevorgalaverdyanmail@gmail.com>
2023-02-15 22:46:40 -0500

    login page added with the navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Header from './components/Header';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <div >
-      Hello World
-    </div>
+    <>
+      <Router>
+        <div className="container">
+          <Header/>
+          <Routes>
+            <Route path={'/'} element={<Login/>}/>
+          </Routes>
+        </div>
+      </Router>
+      <ToastContainer/>
+    </>
   );
 }
 
