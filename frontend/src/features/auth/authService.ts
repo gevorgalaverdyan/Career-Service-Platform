4869cbafc8e4efd75323ff6777e6b21da7dcc560
gevorgalaverdyan <gevorgalaverdyanmail@gmail.com>
2023-02-20 23:32:11 -0500

    user can sign in and the API will respond with a token allowing him to remain signed in. implemented redux/axios created custom hook to check login status added private route to restirict access to some pages

--- frontend/src/features/auth/authService.ts
+++ frontend/src/features/auth/authService.ts
@@ -1,9 +1,27 @@
-import axios from 'axios'
+import axios from 'axios';
 
-const API_URL = ''
+const API_URL = '/auth/';
 
 //register
 
 //login
+const login = async (userData: any) => {
+  const response = await axios.post(`${API_URL}login`, userData);
+
+  if (response.data) {
+    localStorage.setItem('user', JSON.stringify(response.data));
+  }
+  return response.data;
+};
 
 //logout
+const logout = () => {
+  localStorage.removeItem('user');
+};
+
+const authService = {
+  logout,
+  login,
+};
+
+export default authService;
--- frontend/src/features/auth/authSlice.ts
+++ frontend/src/features/auth/authSlice.ts
@@ -1,10 +1,12 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import authService from './authService';
 
 //const user = JSON.parse(localStorage.getItem('user'));
+const userJSON = localStorage.getItem('user');
+const user = userJSON !== null ? JSON.parse(userJSON) : '';
 
 const initialState = {
-  //user: user ?? null,
-  user: null,
+  user: user ?? null,
   isError: false,
   isSuccess: false,
   isLoading: false,
@@ -14,16 +16,57 @@ const initialState = {
 //register
 
 //login
+export const login = createAsyncThunk(
+  'auth/login',
+  async (user: any, thunkAPI: any) => {
+    console.log(user);
+
+    try {
+      return await authService.login(user);
+    } catch (error: any) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
 
 //logout
 
 export const authSlice = createSlice({
-    name: 'auth',
-    initialState,
-    reducers: {},
-    extraReducers: (builder) => {
-        //register fulfilled ...
-    }
-})
+  name: 'auth',
+  initialState,
+  reducers: {
+    reset: (state) => {
+      state.isError = false;
+      state.isSuccess = false;
+      state.isLoading = false;
+      state.message = '';
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(login.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.user = action.payload;
+      })
+      .addCase(login.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message =
+          typeof action.payload === 'string' ? action.payload : '';
+        state.user = null;
+      });
+  },
+});
 
-export default authSlice.reducer
+export default authSlice.reducer;
