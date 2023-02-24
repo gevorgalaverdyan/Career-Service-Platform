71e7209423eeea054858701b7bbf4d059598900b
gevorgalaverdyan <gevorgalaverdyanmail@gmail.com>
2023-02-24 12:10:40 -0500

    added test descriptions for Jest

--- frontend/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Main from './pages/Main';
-
-test('renders learn react link', () => {
-  render(<Main />);
-  const linkElement = screen.getByText(/Main/i);
-  expect(linkElement).toBeDefined();
-});
