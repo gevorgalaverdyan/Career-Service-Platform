16dd2253f4762bf1314d66adcc000a425872897f
gevorgalaverdyan <gevorgalaverdyanmail@gmail.com>
2023-02-23 21:44:08 -0500

    setting up Jest for test cases

---
+++ backend/__tests__/user.test.ts
@@ -0,0 +1,5 @@
+//import what we want to test
+describe('user can log in', ()=>{
+    it.todo('login existing user');
+
+});
--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Main from './pages/Main';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  render(<Main />);
+  const linkElement = screen.getByText(/Main/i);
+  expect(linkElement).toBeDefined();
 });
