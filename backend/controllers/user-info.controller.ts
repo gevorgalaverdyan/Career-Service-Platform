471a64a0550d2e55c03c0a79a77487d81e524c6a
gevorgalaverdyan <gevorgalaverdyanmail@gmail.com>
2023-03-01 15:57:34 -0500

    fixing updateUser to return newest one

--- backend/controllers/user-info.controller.ts
+++ backend/controllers/user-info.controller.ts
@@ -25,33 +25,23 @@ const getUserById = async (req, res) => {
         email: user.email,
       });
     });
-
 };
 
 const updateUser = async (req, res) => {
-    console.log(req.param.id);
-    User.findOneAndUpdate(
-        {_id:req.params.id},
-        req.body
-    )
+  console.log(req.params.id);
+  User.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
     .populate('roles', '-__v')
     .exec((err, user) => {
-    if (err) {
-    res.status(500).send({ message: err });
-    return;
-    }
+      if (err) {
+        res.status(500).send({ message: err });
+        return;
+      }
 
-    if (!user) {
-    return res.status(404).send({ message: 'User Not Found' });
-    }
- 
-    res.status(200).json({
-      _id: user._id,
-      firstName: user.firstName,
-      lastName: user.lastName,
-      email: user.email,
+      if (!user) {
+        return res.status(404).send({ message: 'User Not Found' });
+      }
+
+      res.status(200).send(user);
     });
-});
-
-}
+};
 module.exports = { getUserById, updateUser };
