052b7d773daed40dc2e0c0c237fae9dc6c0fd10e
Gevorg <gevorgalaverdyanmail@gail.com>
2023-03-01 14:30:08 -0500

    working on test cases

--- backend/__tests__/user.test.ts
+++ backend/__tests__/user.test.ts
@@ -1,10 +1,25 @@
 //import what we want to test
-describe('user can log in and access his profile', () => {
+//@desc user can log in and access his profile
+describe('User', () => {
+  const user = {
+    email: 'a@gmail.com',
+    password: 'a',
+  };
+
+  describe('get User route', () => {
+    describe('given user does not exist', () => {
+      it('should return 404', () => {
+        expect(true).toBe(true);
+      });
+    });
+  });
+
   it.todo('Go to CareersConcordia, login existing user {email, password}');
   it.todo('Open User Profile page');
   it.todo('User should see his information');
 });
 
+/////Afif's test
 describe('user can signup', () => {
   it.todo(
     'Go to CareersConcordia, expected: user can access only login&signup'
