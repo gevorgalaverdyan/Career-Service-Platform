756e5646dd76a979ee688c3f69647c05245edb93
gevorgalaverdyan <gevorgalaverdyanmail@gmail.com>
2023-03-16 21:43:34 -0400

    build test CI

--- backend/__tests__/user.test.ts
+++ backend/__tests__/user.test.ts
@@ -1,37 +1,15 @@
-// //import what we want to test
-// describe('user can log in and access his profile', () => {
-//   it.todo('Go to CareersConcordia, login existing user {email, password}');
-//   it.todo('Open User Profile page');
-//   it.todo('User should see his information');
-// });
-
-// describe('user can signup', () => {
-//   it.todo(
-//     'Go to CareersConcordia, expected: user can access only login&signup'
-//   );
-//   it.todo(
-//     'After typing valid information, user is signed up and exists in the DB'
-//   );
-// });
-describe('Filter function', () => {
-  test('it should filter by a search term (link)', () => {
-    const input = [
-      { id: 1, url: 'https://www.url1.dev' },
-      { id: 2, url: 'https://www.url2.dev' },
-      { id: 3, url: 'https://www.link3.dev' },
-    ];
-
-    const output = [{ id: 3, url: 'https://www.link3.dev' }];
-
-    expect(filterByTerm(input, 'link')).toEqual(output);
-
-    expect(filterByTerm(input, 'LINK')).toEqual(output);
-  });
+//import what we want to test
+describe('user can log in and access his profile', () => {
+  it.todo('Go to CareersConcordia, login existing user {email, password}');
+  it.todo('Open User Profile page');
+  it.todo('User should see his information');
 });
 
-function filterByTerm(inputArr, searchTerm) {
-  const regex = new RegExp(searchTerm, 'i');
-  return inputArr.filter(function (arrayElement) {
-    return arrayElement.url.match(regex);
-  });
-}
+describe('user can signup', () => {
+  it.todo(
+    'Go to CareersConcordia, expected: user can access only login&signup'
+  );
+  it.todo(
+    'After typing valid information, user is signed up and exists in the DB'
+  );
+});
