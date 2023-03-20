649c0acb84d8320597376d1cf0cbfb11f05289d6
gevorgalaverdyan <gevorgalaverdyanmail@gmail.com>
2023-03-20 16:21:50 -0400

    working on Service

--- frontend/src/features/application/applicationService.ts
+++ frontend/src/features/application/applicationService.ts
@@ -4,6 +4,9 @@ import axios from 'axios';
 const API_URL = '/auth/';
 
 //add
+const createApplication = async (applicationIDs:any) => {
+    const res = await axios.post(applicationIDs)
+}
 //get
 //delete
 const applicationService={}
