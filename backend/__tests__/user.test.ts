baca4522e3e14925022e66bcd0be6906557d644f
Cristian Gasparesc (CritixGames) <93558364+CritixGames@users.noreply.github.com>
2023-03-24 02:36:43 -0400

    Update User Test (Create a Posting)

--- backend/__tests__/user.test.ts
+++ backend/__tests__/user.test.ts
@@ -40,6 +40,51 @@ describe('login to profile', () => {
   });
 });
 
+//////Cris Test
+describe('create job', () => {
+  it.todo('A recruiter should be able to create a job posting');
+  it.todo('Sign in as a company and then create a posting ');
+  it.todo('A user(Job Seeking user) should see the posting with all the details')
+});
+  
+  beforeAll(async () => {
+    const mongoServer = await MongoMemoryServer.create();
+    await mongoose.connect(mongoServer.getUri());
+  });
+
+  describe('get login route', () => {
+    describe('given user does not exist', () => {
+      it('should return 404', async () => {
+        const userData = {
+          email: 'testJest@gmail.com',
+          password: '123',
+        };
+
+        await supertest(app).post(`${API_URL}login`).send(userData).expect(404);
+      })
+    })
+  });
+
+  describe('Post Job Posting', () => {
+    describe('given field left empty', () => {
+      it('should return 404', async () => {
+    const postingData = {
+      CompanyName: 'Genetec',
+      Description: 'Full Stack, 12$/h,etc..',
+      Deadline: '10/10/2009'
+
+  };
+  await supertest(app).post(`${API_URL}postPosting`).send(postingData).expect(404);
+  })})});
+  
+
+  afterAll(async () => {
+    await mongoose.disconnect();
+    await mongoose.connection.close();
+
+});
+
+
 /////Afif's test
 describe('user can signup', () => {
   it.todo(
